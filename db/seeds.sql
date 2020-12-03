afd7592e0f5767d41454a889fbd9f239f73a16dd
bcshanken <bcshanken@gmail.com>
2020-12-03 14:20:15 -0500

    added file sturcture and db schema/seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('BlackBean Burger');
+INSERT INTO burgers (name) VALUES ('Spicy Southwest Burger');
+INSERT INTO burgers (name) VALUES ('All American Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Sweet Teryaki Burger');
+INSERT INTO burgers (name) VALUES ('Chop House Burger');

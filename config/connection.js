2182b08e7db5a6aa3294fdbc615a817e65829fe6
bcshanken <bcshanken@gmail.com>
2021-01-25 14:58:53 -0500

    adding jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "ShenTP4$",
+//   database: "burgers_db"
+// });
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "ShenTP4$",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+} else {connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'ShenTP4$',
+  database: 'urgers_db'
+})}
 
 // Make connection.
 connection.connect(function(err) {
